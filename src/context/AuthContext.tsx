3078e97be48f76b23ec5ffdc863e079bdd7d7697
TadanobuTokiwa <tokiwa_tadanobu@rext.work>
2024-11-21 17:24:09 +0900

    AuthProviderの作成

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { onAuthStateChanged, User } from 'firebase/auth';
+import { getFirebaseServices } from '@/lib/firebase/firebaseConfig';
+
+type AuthContextType = {
+    userName: string | null;
+    user: User | null;
+    loading: boolean;
+};
+
+const AuthContext = createContext<AuthContextType>({ userName: null, user: null, loading: true });
+
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    const [userName, setUserName] = useState<string | null>(null);
+    const [user, setUser] = useState<User | null>(null);
+    const [loading, setLoading] = useState(true);
+
+    const { auth } = getFirebaseServices();
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
+            setUser(currentUser);
+            setUserName(currentUser?.displayName || null);
+            setLoading(false);
+        });
+        return () => unsubscribe();
+    }, [auth]);
+
+    return (
+        <AuthContext.Provider value={{ userName, user, loading }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth = () => useContext(AuthContext);
